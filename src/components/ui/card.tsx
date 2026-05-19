fe2dd94e290542d31bd8655a186ac5531f5a81fd
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-05-19 18:45:05 +0530

    Add src/components/ui/card.tsx

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import { cn } from "../../lib/utils";
+
+export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("surface", className)} {...props} />;
+}
+
+export function CardHeader({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("border-b border-border p-4", className)} {...props} />;
+}
+
+export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("p-4", className)} {...props} />;
+}
